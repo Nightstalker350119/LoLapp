63f74f2ade1c0a6e64bf2e313e175cf1c9d96dbf
Norberto Taveras <nm.taveras@gmail.com>
2017-11-28 10:08:27 -0500

    Riot Games API Calls Declarations
    - LeagueItemDTO
    - LeagueListDTO
    - LeaguePositionDTO
    - MiniSeriesDTO

---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/LeagueListDTO.java
@@ -0,0 +1,15 @@
+package com.norbertotaveras.game_companion_app;
+
+import java.util.List;
+
+/**
+ * Created by Norberto Taveras on 11/28/2017.
+ */
+
+public class LeagueListDTO {
+    public String leagueid;
+    public String tier;
+    public String queue;
+    public String name;
+    public List<LeagueItemDTO> entries;
+}
---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/LeaguePositionDTO.java
@@ -0,0 +1,19 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Norberto on 11/28/2017.
+ */
+
+public class LeaguePositionDTO {
+    public String rank;
+    public String playerOrTeamName;
+    public String playerOrTeamId;
+    public boolean hotStreak;
+    public boolean veteran;
+    public boolean freshBlood;
+    public boolean inactive;
+    public int wins;
+    public int leaguePoints;
+    public int losses;
+    public MiniSeriesDTO miniSeries;
+}
