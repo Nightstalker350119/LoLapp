230f19c22dd703877d1ed658b3e8c424442dc0f9
Emanuel <emanuelantablin@protonmail.com>
2017-12-07 11:52:44 -0500

    Added missing files

---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/ChampionsBanRate.java
@@ -0,0 +1,21 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Emanuel on 12/7/2017.
+ */
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ChampionsBanRate extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_championstbanrate, container, false);
+        return rootView;
+    }
+}
