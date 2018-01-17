7e0ed17814193039b87ad887748ffcf2af3a5394
Norberto Taveras <nm.taveras@gmail.com>
2018-01-17 10:00:33 -0500

    Updated Repo.
    - Commit to update the repo.

---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/RateLimiter.java
@@ -0,0 +1,8 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Norberto on 1/11/2018.
+ */
+
+public class RateLimiter {
+}
---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/RecentSearchStorage.java
@@ -0,0 +1,8 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Norberto on 1/16/2018.
+ */
+
+public class RecentSearchStorage {
+}
