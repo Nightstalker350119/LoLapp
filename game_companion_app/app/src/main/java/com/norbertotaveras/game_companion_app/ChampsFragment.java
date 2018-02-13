02cc9acb515d9c3d8d3ae1b744595d7cf333d863
Norberto Taveras <nm.taveras@gmail.com>
2018-02-12 19:03:19 -0500

    Major UI Improvements #2
    - Reimplemented the Matches Fragment to work with the new and improved UI.
    - Created and implemented FloatingActionButton
    - Created Matches Filter
    - Changed color scheme/palette of the application.

---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/ChampsFragment.java
@@ -0,0 +1,8 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Norberto on 2/7/2018.
+ */
+
+public class ChampsFragment {
+}
---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/SummonerSearchFilterFab.java
@@ -0,0 +1,8 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Norberto on 2/2/2018.
+ */
+
+public class SummonerSearchFilterFab {
+}
