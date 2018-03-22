6452a27b9536d9055a09f16bb8acce5d2d31a44e
Norberto Taveras <nm.taveras@gmail.com>
2018-03-22 11:08:31 -0400

    - Created and added the BackgroundNotify interface. - It sets the background for the champion drawable source.

---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/BackgroundNotify.java
@@ -0,0 +1,11 @@
+package com.norbertotaveras.game_companion_app;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Norberto Taveras on 3/14/2018.
+ */
+
+public interface BackgroundNotify {
+    void setBackground(Drawable background);
+}
