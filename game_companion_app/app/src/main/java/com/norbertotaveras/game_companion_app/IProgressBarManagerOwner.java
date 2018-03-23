a7d788da94e98c76da0438996f6e4caeb1dadc41
Norberto Taveras <nm.taveras@gmail.com>
2018-03-23 00:30:14 -0400

    - IProgressBarManagerOwner public interface created. - The interface calls and invokes the getProgressBarManager helper class.

---
+++ game_companion_app/app/src/main/java/com/norbertotaveras/game_companion_app/IProgressBarManagerOwner.java
@@ -0,0 +1,9 @@
+package com.norbertotaveras.game_companion_app;
+
+/**
+ * Created by Norberto Taveras on 3/23/2018.
+ */
+
+public interface IProgressBarManagerOwner {
+    ProgressBarManager getProgressBarManager();
+}
